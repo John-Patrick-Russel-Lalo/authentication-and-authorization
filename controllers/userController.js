d17676d2b5173b6905a87e50d63620659e58f401
John-Patrick-Russel-Lalo <johnpatricklalo@gmail.com>
2026-05-15 03:03:20 +0800

    system v1

---
+++ controllers/userController.js
@@ -0,0 +1,47 @@
+import pool from "../config/db.js";
+
+export const assignRole = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { role } = req.body;
+
+    const allowedRoles = ["patient", "doctor", "staff", "admin"];
+
+    if (!allowedRoles.includes(role)) {
+      return res.status(400).json({
+        message: "Invalid role",
+      });
+    }
+
+    if (req.user.id === userId) {
+      return res.status(400).json({
+        message: "You cannot modify your own role",
+      });
+    }
+
+    
+
+    const result = await pool.query(
+      `UPDATE users
+             SET role = $1
+             WHERE id = $2
+             RETURNING *`,
+      [role, userId],
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Role updated successfully",
+      user: result.rows[0],
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
---
+++ routes/userRoutes.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import { roleMiddleware } from "../middleware/roleMiddleware.js";
+import { assignRole } from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.get(
+    "/admin-only",
+    authMiddleware,
+    roleMiddleware(["admin"]),
+    (req, res) => {
+        res.json({
+            message: "Welcome Admin",
+            user: req.user
+        });
+    }
+);
+
+router.get(
+    "/doctor-area",
+    authMiddleware,
+    roleMiddleware(["doctor", "admin"]),
+    (req, res) => {
+        res.json({
+            message: "Doctor dashboard",
+            user: req.user
+        });
+    }
+);
+
+router.patch(
+    "/users/:userId/role",
+    authMiddleware,
+    roleMiddleware(["admin"]),
+    assignRole
+);
+
+export default router;
