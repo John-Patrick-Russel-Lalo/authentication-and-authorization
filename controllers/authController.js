66ded38c546a40a606c554683dc3ef6d47dd3350
John-Patrick-Russel-Lalo <johnpatricklalo@gmail.com>
2026-05-16 22:20:36 +0800

    remove cookie method

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,18 +34,12 @@ export const login = async (req, res) => {
         role: user.role,
       },
       process.env.JWT_SECRET,
-      { expiresIn: "1d" },
+      { expiresIn: "1d" }
     );
-
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "None",
-      maxAge: 24 * 60 * 60 * 1000,
-    });
 
     res.status(200).json({
       message: "Login successful",
+      token: token,
     });
 
   } catch (err) {
@@ -55,10 +49,4 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = (req, res) => {
-    res.clearCookie("token");
 
-    res.status(200).json({
-        message: "Logged out"
-    });
-};
