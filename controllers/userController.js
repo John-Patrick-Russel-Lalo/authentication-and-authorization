d35433d138e01a971c5cdfcf90bf6dc9800356c5
HazelAnnManimtim <23-12141@g.batstate-u.edu.ph>
2026-05-21 22:17:38 +0800

    Add deleteUser function to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -76,4 +76,25 @@ export const getUserById = async (req, res) => {
         });
     }
 };
+export const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const success = await deleteUserById(id);
+
+        if (!success) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "User deleted successfully"
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        });
+    }
+};
 
